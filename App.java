057f8b0b524d7794562ac57b374eedbef03c2cd0
Alice <alice@perry.nl>
2021-06-22 22:12:29 +0200

    alles werkend muv counter

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        Driving gui = new Driving(2, 2);
+        Driving gui = new Driving(2,2);
         gui.setVisible(true);
     
     }
--- Driving.java
+++ Driving.java
@@ -1,61 +1,74 @@
 import javax.swing.JFrame;
-//import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.Font;
 
-import javax.swing.JButton;
 
-public class Driving extends JFrame implements ActionListener{
+public class Driving extends JFrame implements ActionListener {
     
     public static final int WIDTH = 500;
-    public static final int HEIGHT = 500;
+    public static final int HEIGHT = 150;
 
     JFrame frame;
     JButton yesButton;
     JButton noButton;
     JLabel counterLabel;
     JLabel textLabel;
+    JPanel panel;
 
     public Driving(int rows, int columns) {
 
         super();
                 
         frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Are you a good driver?");
-        frame.setVisible(true);
-
-        setSize(WIDTH, HEIGHT);
-        setLayout (new GridLayout(rows, columns));
-
         yesButton = new JButton("Yes");
-        yesButton.addActionListener(this);
-        add(yesButton);
-        
         noButton = new JButton("No");
+        counterLabel = new JLabel("Counting", SwingConstants.CENTER);
+        textLabel = new JLabel("Are you a good driver?", SwingConstants.CENTER);
+        panel = new JPanel();
+        counterLabel.setFont(new Font("Verdana", Font.PLAIN, 20));
+        textLabel.setFont(new Font("Verdana", Font.BOLD, 30));
+
+        frame.setSize(WIDTH, HEIGHT);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setTitle("Are you a good driver?");
+        frame.setLayout(new BorderLayout());
+        panel.setLayout(new GridLayout(1,2));
+
+        yesButton.addActionListener(this);
         noButton.addActionListener(this);
-        add(noButton);
 
-        textLabel = new JLabel("Are you an excellent driver?", SwingConstants.CENTER);
-        add(textLabel);
-        yesButton.add(textLabel);
-        
-        counterLabel = new JLabel("Yes or no counter:", SwingConstants.CENTER);
-        add(counterLabel);
-        
 
+        frame.add(textLabel, BorderLayout.NORTH);
+        frame.add(panel, BorderLayout.CENTER);
+        frame.add(counterLabel, BorderLayout.SOUTH);
         
         
+        panel.add(yesButton);
+        panel.add(noButton);
+        
+        frame.setVisible(true);
     }
 
-    public void actionPerformed(ActionEvent event) {
-       textLabel.setText("textlabel");
+    public void actionPerformed(ActionEvent e) {
+        String buttonString = e.getActionCommand();
 
-
-        //yesButton.setText(“I’ve been clicked!”);    }
-}
+        if (buttonString.equals("Yes")){
+            textLabel.setText("WE NEED YOU!");
+        }
+        else if (buttonString.equals("No")){
+            textLabel.setText("YOU NEED TO PRACTICE.");
+        }
+        else{
+            System.out.println("OEPS");
+        }
+        
+    }
 }
